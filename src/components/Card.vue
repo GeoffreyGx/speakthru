75aa7d0214c9794f582e25c13ef06dc8d038734d
GeoffreyGx <geoffrey.gambicchia@outlook.com>
2024-06-02 21:07:47 +0200

    Updating

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        <img>
+        <div>
+            <p>Name</p>
+            <p>Status</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.card {
+    height: 100px;
+    width: 400px;
+    background-color: aquamarine;
+
+    border: 2px solid aquamarine;
+    border-radius: 10px;
+}
+</style>
--- src/views/Call.vue
+++ src/views/Call.vue
@@ -1,6 +1,45 @@
+<script setup lang="ts">
+import Card from '../components/Card.vue';
+</script>
+
 <template>
     <main class="call-page">
-        <h1>Call</h1>
-        <p>This is the call page</p>
+        <h1 class="page-title">Call</h1>
+        <div class="grid-container">
+            <div class="grid-item">
+                <Card />
+            </div>
+            <div class="grid-item">
+                <Card />
+            </div>
+            <div class="grid-item">
+                <Card />
+            </div>
+            <div class="grid-item">
+                <Card />
+            </div>
+            <div class="grid-item">
+                <Card />
+            </div>
+            <div class="grid-item">
+                <Card />
+            </div>
+            <div class="grid-item">
+                <Card />
+            </div>
+            <div class="grid-item">
+                <Card />
+            </div>
+        </div>
     </main>
 </template>
+
+<style>
+.page-title {
+    padding-bottom: 10px
+}
+
+.grid-container {
+    display: grid;
+}
+</style>
